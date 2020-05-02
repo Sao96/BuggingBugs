97ebccc0aff439afbe011fdf5d04d9455a89087a
Sao96 <saothman96@gmail.com>
2020-05-02 09:24:01 -0700

    Work done on Ticket component. Supports a renderer for a PFP, sender name, progress code, priority, and due date

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devServer: {
+    historyApiFallback: true
+  },
+  entry: {
+    main: path.join(__dirname, "src", "index.js")
+  },
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "[name].js"
+  },
+  optimization: {
+    removeAvailableModules: false,
+    removeEmptyChunks: false,
+    splitChunks: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.ttf$/,
+        use: [
+          {
+            loader: "ttf-loader",
+            options: {
+              name: "./font/[hash].[ext]"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|woff|woff2|eot)$/,
+        loader: "file-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.join(__dirname, "src", "index.html")
+    })
+  ]
+};
