984a1e453a02fae5b0b26b402800607ec1cc6700
Sao96 <saothman@ucdavis.edu>
2020-05-03 03:09:09 -0700

    Development on ticket cards. Added all colors and dynamic settings through properties of ticket.

--- src/app.jsx
+++ src/app.jsx
@@ -9,20 +9,51 @@ export default class App extends Component {
   }
   render() {
     const ticketProps = {
-      pfp: "https://i.imgur.com/JBQcxgY.png",
+      pfp: "https://i.imgur.com/lbrVhd3.png",
       author: "Scott Combs",
-      priority: 1,
+      priority: 0,
       status: 1,
       duedate: "5/20",
+      summary:
+        "Strings sent to website end up being corrupted when displayed afterward. Text too close to borders.",
+    };
+    const ticketProps2 = {
+      pfp:
+        "https://i.pinimg.com/474x/a9/54/78/a95478bbf2d5520fae91a9b95e321122.jpg",
+      author: "Andrew Williams",
+      priority: 1,
+      status: 1,
+      duedate: "5/21",
+      summary:
+        "Vanguard system causes the entire system to enter a deadlock state. Harddrive must be formatted for system to run back on.",
+    };
+
+    const ticketProps3 = {
+      pfp: "https://i.imgur.com/poZr1ed.png",
+      author: "James Jones",
+      priority: 2,
+      status: 1,
+      duedate: "5/24",
+      summary:
+        "Display photos on the home feed get mixed with other users, homepage displays random photo's from the internet.",
+    };
+
+    const ticketProps4 = {
+      pfp: "https://i.imgur.com/aIBs6cj.png",
+      author: "Michael Toms",
+      priority: 3,
+      status: 1,
+      duedate: "5/25",
+      summary:
+        "Display photos on the home feed get mixed with other users, homepage displays random photo's from the internet.",
     };
 
     return (
       <div class="main">
-        {/* <Sidebar /> */}
-        {/* <Sidebar></Sidebar> */}
-        <Ticket {...ticketProps} />
-        <Ticket {...ticketProps} />
-        <Ticket {...ticketProps} />
+        <Ticket {...ticketProps} status={0} />
+        <Ticket {...ticketProps2} status={1} />
+        <Ticket {...ticketProps3} status={2} />
+        <Ticket {...ticketProps4} status={1} />
       </div>
     );
   }
--- src/components/pins/ticket.jsx
+++ src/components/pins/ticket.jsx
@@ -1,43 +1,75 @@
 import React from "react";
+import Open from "../../../svg/open.svg";
 import Worker from "../../../svg/worker.svg";
+import Closed from "../../../svg/closed.svg";
 
 export default function Ticket(props) {
   const Separator = (color) => {
     const styles = {
       border: "1px solid " + color,
       width: "90%",
-      marginTop: "5px",
-      marginBottom: "5px",
+      marginBottom: "7px",
     };
     return <div style={styles}> </div>;
   };
 
+  let bgColor, borderColor, separatorColor;
+  switch (props.priority) {
+    case 0: //max priority
+      bgColor = "rgb(150,0,.5)";
+      borderColor = "rgb(50, 0, 0)";
+      separatorColor = "red";
+      break;
+    case 1:
+      bgColor = "rgb(185, 84, 0 )";
+      borderColor = "rgb(59, 30, 0)";
+      separatorColor = "rgb(255, 116, 0 )";
+      break;
+    case 2: //high priority
+      bgColor = "rgb(101, 118, 0)";
+      borderColor = "rgb(54, 64, 0)";
+      separatorColor = "rgb(172, 202, 0 )";
+      break;
+    case 3:
+      bgColor = "rgb(0, 152, 143 )";
+      borderColor = "rgb(0, 126, 118 )";
+      separatorColor = "rgb(0, 234, 219  )";
+  }
+
   const ticketStyle = {
     //primary style of tickets
-    width: "290px",
-    height: "210px",
+    width: "360px",
+    height: "230px",
     borderRadius: "10px",
-    backgroundColor: "rgb(255,0,0,.5)",
+    backgroundColor: bgColor,
+    border: "0.5px solid " + borderColor,
     color: "white",
     marginTop: "5px",
-    padding: "2px 0 0 2px",
-    border: "0.5px solid maroon",
+    padding: "4px 0px 0px 2px",
     boxShadow: "10px 10px 23px -6px rgba(0,0,0,0.75)",
-    fontFamily: "Quattrocento Sans",
-    fontSize: "20px",
-    justifyContent: "center",
+    fontFamily: "Didact Gothic, Quattrocento Sans",
+    fontSize: "18px",
     position: "relative",
   };
 
   const SizeSVG = (Comp) => {
-    return <Comp style={{ width: "50px", height: "65px" }} />;
+    return <Comp style={{ fill: "white", width: "50px", height: "65px" }} />;
   };
   const PFPImage = (image) => {
     return (
-      <img
-        style={{ height: "40px", width: "40px", borderRadius: "50%" }}
-        src={image}
-      />
+      <span style={{ paddingLeft: "10px" }}>
+        <img
+          style={{
+            height: "40px",
+            width: "40px",
+            borderRadius: "50%",
+            borderStyle: "solid",
+            borderColor: "black",
+            borderWidth: "1px",
+          }}
+          src={image}
+        />
+      </span>
     );
   };
   const Sender = (name) => {
@@ -56,12 +88,37 @@ export default function Ticket(props) {
       </span>
     );
   };
+  const SummaryText = (text) => {
+    return (
+      <span
+        style={{
+          textAlign: "center",
+          position: "absolute",
+          margin: "0px 3px 0px",
+        }}
+      >
+        {text}
+      </span>
+    );
+  };
+  const StatusIcon = (status) => {
+    let Comp;
+    if (status == 0) Comp = Open;
+    else if (status == 1) Comp = Worker;
+    else Comp = Closed;
+
+    return (
+      <span style={{ position: "absolute", left: "1%", top: "77%" }}>
+        {SizeSVG(Comp)}
+      </span>
+    );
+  };
   const DueDate = (date) => {
     return (
       <span
         style={{
           position: "absolute",
-          right: "2%",
+          right: "3%",
           top: "90%",
           fontStyle: "italic",
           color: "rgb(200,200,200)",
@@ -71,21 +128,14 @@ export default function Ticket(props) {
       </span>
     );
   };
-  const StatusIcon = (status) => {
-    return (
-      <span style={{ position: "absolute", left: "-1%", top: "75%" }}>
-        {SizeSVG(Worker)}
-      </span>
-    );
-  };
 
   return (
     <div style={ticketStyle}>
       <div>
         {PFPImage(props.pfp)}
         {Sender(props.author)}
-        {Separator(props.priority)}
-        Strings sent to website end up being corrupted when displayed afterward
+        {Separator(separatorColor)}
+        {SummaryText(props.summary)}
         {StatusIcon(props.status)}
         {DueDate(props.duedate)}
       </div>
