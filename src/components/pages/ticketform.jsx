d2663c682fed91d7ffe20d96f3d3f3f930c6b869
Sao96 <saothman@ucdavis.edu>
2020-05-07 02:09:23 -0700

    Development on adding comments section to tickets

--- src/components/pages/ticketform.jsx
+++ src/components/pages/ticketform.jsx
@@ -4,7 +4,56 @@ function attachment(props) {
   //pic, name, attachment
 }
 
-function comment() {}
+const sample = {
+  pfp: "https://i.imgur.com/aIBs6cj.png",
+  name: "Smithy Jones",
+  message:
+    "Display photos on the home feed get mixed with other users, homepage displays random photo's from the internet. Display photos on the home feed get mixed with other users, homepage displays random photo's from the internet.",
+  date: "May 7, 2019, 12:19 PST",
+};
+
+const sample2 = {
+  pfp: "https://i.imgur.com/aIBs6cj.png",
+  name: "Smithy Jones",
+  message:
+    "Display photos on the home feed get mixed with other users, homepage displays random photo's from the internet. ",
+  date: "May 7, 2019, 12:19 PST",
+};
+function CommentBox(props) {}
+
+function Comment(props) {
+  const commentStyle = {
+    backgroundColor: "orange",
+    paddingLeft: "10px",
+  };
+  const imgStyle = {
+    height: "40px",
+    width: "40px",
+    borderRadius: "50%",
+    borderStyle: "solid",
+    borderColor: "black",
+    borderWidth: "1px",
+    marginRight: "5px",
+  };
+  return (
+    <div style={{ display: "flex" }}>
+      <img style={imgStyle} src={props.pfp} />
+      <div style={commentStyle}>
+        <span style={{ fontSize: "16px", paddingRight: "15px" }}>
+          {" "}
+          {props.name}
+        </span>
+        <span style={{ fontSize: "14px" }}>{props.date}</span>
+        <div style={{ flexBasis: "100%", height: "5px" }}></div>
+        <span
+          style={{ position: "relative", display: "flex", flexWrap: "wrap" }}
+        >
+          {props.message}
+        </span>
+      </div>
+    </div>
+  );
+}
 
 export default class TicketForm extends Component {
   constructor(props) {
@@ -64,11 +113,19 @@ export default class TicketForm extends Component {
 
   DiscussionSection() {
     // return <div>fill me later</div>;
+    return <div></div>;
   }
 
   render() {
     return (
-      <div style={{ backgroundColor: "white" }}>{this.TicketInfoSection()}</div>
+      <div style={{ backgroundColor: "white" }}>
+        {this.TicketInfoSection()}
+        <h1>Comments</h1>
+        <Comment {...sample} />
+        <br></br>
+        <Comment {...sample2} />
+        {this.DiscussionSection()}
+      </div>
     );
   }
 }
