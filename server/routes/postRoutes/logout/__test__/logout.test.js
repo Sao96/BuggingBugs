3447c078993d9c062fee44a6e3f9e6fd657211f6
sao96 <saothman@ucdavis.edu>
2020-06-24 08:09:14 -0700

    Complete testing for logout

--- server/routes/postRoutes/logout/__test__/logout.test.js
+++ server/routes/postRoutes/logout/__test__/logout.test.js
@@ -2,18 +2,18 @@ import "babel-polyfill";
 import { domain } from "domain.js";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
-import {} from "models";
 
 dotenv.config();
+const testEmail1 = process.env.TESTEMAIL1;
+const testPassword = process.env.TESTPASSWORD;
 const TIMEOUT = 20000;
-const testEndpoint = domain + "amilogged";
-const testemail1 = process.env.TESTEMAIL1;
-const testpassword = process.env.TESTPASSWORD;
-
+const logoutEndpoint = domain + "logout";
+const amiloggedEndpoint = domain + "amilogged";
+const loginEndpoint = domain + "login";
 test(
     "Get a false response when not logged in",
     async () => {
-        const res = await fetch(testEndpoint, {
+        const res = await fetch(amiloggedEndpoint, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -35,12 +35,11 @@ test(
     "Login & Get Session for user1",
     async () => {
         const loginInfo = {
-            email: testemail1,
-            password: testpassword,
+            email: testEmail1,
+            password: testPassword,
             type: "native",
         };
 
-        const loginEndpoint = domain + "login";
         const loginRes = await fetch(loginEndpoint, {
             method: "POST",
             headers: {
@@ -59,9 +58,9 @@ test(
 );
 
 test(
-    "Get a true response when logged in",
+    "Get a true response when not logged in",
     async () => {
-        const res = await fetch(testEndpoint, {
+        const res = await fetch(amiloggedEndpoint, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -75,6 +74,45 @@ test(
         expect(res.status).toBe(200);
         const amILoggedRes = await res.json();
         expect(amILoggedRes.loggedIn).toBe(true);
+    },
+    TIMEOUT
+);
+
+test(
+    "Request logout to server",
+    async () => {
+        const logoutRes = await fetch(logoutEndpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                Cookie: sessionCookie1,
+            },
+            credentials: "include",
+            mode: "cors",
+            cache: "no-cache",
+        });
+        expect(logoutRes.status).toBe(200);
+    },
+    TIMEOUT
+);
+
+test(
+    "Get a false response when not logged in",
+    async () => {
+        const res = await fetch(amiloggedEndpoint, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            credentials: "include",
+            mode: "cors",
+            cache: "no-cache",
+        });
+        expect(res.status).toBe(200);
+        const amILoggedRes = await res.json();
+        expect(amILoggedRes.loggedIn).toBe(false);
     },
     TIMEOUT
 );
