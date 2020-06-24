d7cc9e5ad0119d502e3f53084baf7298e1bef077
Sao96 <saothman@ucdavis.edu>
2020-06-24 06:39:19 -0700

    complete native login testing

---
+++ server/routes/getRoutes/login/__test__/login.test.js
@@ -0,0 +1,132 @@
+import "babel-polyfill";
+import { domain } from "domain.js";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+
+dotenv.config();
+const testEmail1 = process.env.TESTEMAIL1;
+const testPassword = process.env.TESTPASSWORD;
+const TIMEOUT = 20000;
+const testEndpoint = domain + "login";
+
+test("Bad login type fails", async () => {
+    const reqData = { type: "test" }
+    const res = await fetch(testEndpoint, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        credentials: "include",
+        mode: "cors",
+        cache: "no-cache",
+        body: JSON.stringify(reqData)
+    });
+    expect(res.status).toBe(400);
+}, TIMEOUT)
+
+test("Bad native email fails", async () => {
+    let reqData = { type: "native", email: 52, password: "avalidpassword123" };
+    let res = await fetch(testEndpoint, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        credentials: "include",
+        mode: "cors",
+        cache: "no-cache",
+        body: JSON.stringify(reqData)
+    });
+    expect(res.status).toBe(400);
+
+    reqData.email = "not.valid.email";
+    res = await fetch(testEndpoint, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        credentials: "include",
+        mode: "cors",
+        cache: "no-cache",
+        body: JSON.stringify(reqData)
+    });
+    expect(res.status).toBe(400);
+
+    reqData.email = "still.not.valid.@email";
+    res = await fetch(testEndpoint, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        credentials: "include",
+        mode: "cors",
+        cache: "no-cache",
+        body: JSON.stringify(reqData)
+    });
+    expect(res.status).toBe(400);
+}, TIMEOUT);
+
+test("Bad native password fails", async () => {
+    let reqData = { type: "native", email: "valid@email.com", password: 52 };
+    let res = await fetch(testEndpoint, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        credentials: "include",
+        mode: "cors",
+        cache: "no-cache",
+        body: JSON.stringify(reqData)
+    });
+    expect(res.status).toBe(400);
+
+    reqData.password = "2short";
+    res = await fetch(testEndpoint, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        credentials: "include",
+        mode: "cors",
+        cache: "no-cache",
+        body: JSON.stringify(reqData)
+    });
+    expect(res.status).toBe(400);
+}, TIMEOUT)
+
+test("Non-existent native credentials fail", async () => {
+    let reqData = { type: "native", email: "valid@email.com", password: "validpassword123" };
+    let res = await fetch(testEndpoint, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        credentials: "include",
+        mode: "cors",
+        cache: "no-cache",
+        body: JSON.stringify(reqData)
+    });
+    expect(res.status).toBe(400);
+}, TIMEOUT)
+
+test("Valid credentials work", async () => {
+    let reqData = { type: "native", email: testEmail1, password: testPassword };
+    let res = await fetch(testEndpoint, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        credentials: "include",
+        mode: "cors",
+        cache: "no-cache",
+        body: JSON.stringify(reqData)
+    });
+    expect(res.status).toBe(200);
+}, TIMEOUT)
